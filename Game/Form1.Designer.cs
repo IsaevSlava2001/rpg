0e6369900044721550b5cbbcfed23467055597a2
IsaevSlava2001 <IsaevSlava.2001@yandex.ru>
2016-02-28 17:53:03 +0300

    1,2

--- Game/Form1.Designer.cs
+++ Game/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.Startgamebutton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Startgamebutton
@@ -41,11 +42,21 @@
             this.Startgamebutton.UseVisualStyleBackColor = true;
             this.Startgamebutton.Click += new System.EventHandler(this.Startgamebutton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(449, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(729, 273);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Startgamebutton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button Startgamebutton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
