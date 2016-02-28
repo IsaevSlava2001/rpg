28fa8af09e189f920509a3f3f02ca82246fdc51f
IsaevSlava2001 <IsaevSlava.2001@yandex.ru>
2016-02-28 18:32:27 +0300

    1.2

--- Game/Form1.Designer.cs
+++ Game/Form1.Designer.cs
@@ -30,6 +30,29 @@
         {
             this.Startgamebutton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Startgamebutton
@@ -44,18 +67,256 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(449, 35);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "палладин";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "жрец";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "вор";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(137, 120);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "шаман";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(642, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "жрец";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(517, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "палладин";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(642, 120);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "шаман";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(520, 120);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "вор";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(9, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(134, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(131, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 23);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(12, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 23);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(12, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 23);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(137, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 23);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(137, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 23);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(517, 51);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 23);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "label9";
+            this.label9.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(517, 86);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 23);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "label10";
+            this.label10.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(642, 51);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(75, 23);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "label11";
+            this.label11.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(642, 86);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 23);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "label12";
+            this.label12.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(520, 157);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(75, 23);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "label13";
+            this.label13.Visible = false;
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(520, 197);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(75, 23);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "label14";
+            this.label14.Visible = false;
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(639, 197);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(75, 23);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "label15";
+            this.label15.Visible = false;
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(642, 157);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(75, 23);
+            this.label16.TabIndex = 24;
+            this.label16.Text = "label16";
+            this.label16.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(729, 273);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Startgamebutton);
             this.Name = "Form1";
@@ -69,6 +330,29 @@
 
         private System.Windows.Forms.Button Startgamebutton;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
     }
 }
 
--- Game/Form1.cs
+++ Game/Form1.cs
@@ -15,20 +15,70 @@ namespace Game
     {
         public Form1()
         {
-            
+            //button1.Visible = false;
+            //button2.Visible = false;
+            //button3.Visible = false;
+            //button4.Visible = false;
+            //button5.Visible = false;
+            //button6.Visible = false;
+            //button7.Visible = false;
+            //button8.Visible = false;
+            //label1.Visible = false;
+            //label2.Visible = false;
+            //label3.Visible = false;
+            //label4.Visible = false;
+            //label5.Visible = false;
+            //label6.Visible = false;
+            //label7.Visible = false;
+            //label8.Visible = false;
+            //label9.Visible = false;
+            //label10.Visible = false;
+            //label11.Visible = false;
+            //label12.Visible = false;
+            //label13.Visible = false;
+            //label14.Visible = false;
+            //label15.Visible = false;
+            //label16.Visible = false;
+
             InitializeComponent();
         }
 
         private void Startgamebutton_Click(object sender, EventArgs e)
         {
-            
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = true;
+            button8.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+            label9.Visible = true;
+            label10.Visible = true;
+            label11.Visible = true;
+            label12.Visible = true;
+            label13.Visible = true;
+            label14.Visible = true;
+            label15.Visible = true;
+            label16.Visible = true;
+
             Startgamebutton.Visible = false;
             
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-              Character Palladin1 = new Palladin(800, 400, 80);
+
+            
+                Character Palladin1 = new Palladin(800, 400, 80);
             Character Palladin2 = new Palladin(800, 400, 80);
             Character Priest1 = new Priest(400, 900, 30);
             Character Priest2 = new Priest(400, 900, 30);
@@ -36,6 +86,43 @@ namespace Game
             Character Shaman2 = new Shaman(500, 700, 50);
             Character Rogue1 = new Rogue(360, 500, 100);
             Character Rogue2 = new Rogue(360, 500, 100);
+            List<Character> AllCharacters = new List<Character>();
+
+            {
+
+
+                AllCharacters.Add(Palladin1);
+                AllCharacters.Add(Palladin2);
+                AllCharacters.Add(Priest1);
+                AllCharacters.Add(Priest2);
+                AllCharacters.Add(Shaman1);
+                AllCharacters.Add(Shaman2);
+                AllCharacters.Add(Rogue1);
+                AllCharacters.Add(Rogue2);
+            }
+            label1.Text = (" " + AllCharacters[0].HP);
+            label2.Text = (" " + AllCharacters[0].MP);
+            label3.Text = (" " + AllCharacters[1].HP);
+            label4.Text = (" " + AllCharacters[1].MP);
+            label5.Text = (" " + AllCharacters[2].HP);
+            label6.Text = (" " + AllCharacters[2].MP);
+            label7.Text = (" " + AllCharacters[3].HP);
+            label8.Text = (" " + AllCharacters[3].MP);
+            label9.Text = (" " + AllCharacters[4].HP);
+            label10.Text = (" " + AllCharacters[4].MP);
+            label11.Text = (" " + AllCharacters[5].HP);
+            label12.Text = (" " + AllCharacters[5].MP);
+            label13.Text = (" " + AllCharacters[6].HP);
+            label14.Text = (" " + AllCharacters[6].MP);
+            label15.Text = (" " + AllCharacters[7].HP);
+            label16.Text= (" " + AllCharacters[7].MP);
+
+            //label1.Text = (" " + AllCharacters[0].HP);
+            //label1.Text = (" " + AllCharacters[0].HP);
+            //label1.Text = (" " + AllCharacters[0].HP);
+            //label1.Text = (" " + AllCharacters[0].HP);
+            //label1.Text = (" " + AllCharacters[0].HP);
+
         }
 
    
